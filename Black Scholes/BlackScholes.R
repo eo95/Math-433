3d36209cf92d417998b7dd20c336310be5348779
garvinorig <99909053+garvinorig@users.noreply.github.com>
2022-02-17 16:17:03 -0500

    Update BlackScholes.R

--- Black Scholes/BlackScholes.R
+++ Black Scholes/BlackScholes.R
@@ -1,14 +1,12 @@
 #Black Scholes with or without dividend yeild
 BlackScholes <- function(S, K, sigma, r, T, delta, type){
   if (type == 0 ){
-    S = So - dividends*exp(-r*t)
     d1 <- (log(S/K) + (r - delta + sigma^2/2)*T) / (sigma*sqrt(T))
     d2 <- d1 - sigma*sqrt(T)
     price <- S * pnorm(d1) - K*exp(-r*T)*pnorm(d2)
     return(price)
   }
   if (type== 1 ){
-    S = So - dividends*exp(-r*t)
     d1 <- (log(S/K) + (r - delta + sigma^2/2)*T) / (sigma*sqrt(T))
     d2 <- d1 - sigma*sqrt(T)
     price <- -S * pnorm(-d1) + K*exp(-r*T)*pnorm(-d2)
@@ -19,3 +17,11 @@ BlackScholes <- function(S, K, sigma, r, T, delta, type){
 #0 is call
 #1 is put
 #This formula does not account for dividends
+
+#Call Premium
+#BlackScholes(41, 40, 0.3, 0.08, 0.25, 0, 0)
+#Should equal 3.399 from textbook
+
+#Put Premium
+#BlackScholes(41, 40, 0.3, 0.08, 0.25, 0, 0)
+#Should equal 1.607
