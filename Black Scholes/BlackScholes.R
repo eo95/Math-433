f7eab463e1225755f290e635fd8ec827e0f9f1ce
garvinorig <99909053+garvinorig@users.noreply.github.com>
2022-02-17 16:12:17 -0500

    Update BlackScholes.R

--- Black Scholes/BlackScholes.R
+++ Black Scholes/BlackScholes.R
@@ -1,12 +1,14 @@
 #Black Scholes with or without dividend yeild
 BlackScholes <- function(S, K, sigma, r, T, delta, type){
   if (type == 0 ){
+    S = So - dividends*exp(-r*t)
     d1 <- (log(S/K) + (r - delta + sigma^2/2)*T) / (sigma*sqrt(T))
     d2 <- d1 - sigma*sqrt(T)
     price <- S * pnorm(d1) - K*exp(-r*T)*pnorm(d2)
     return(price)
   }
   if (type== 1 ){
+    S = So - dividends*exp(-r*t)
     d1 <- (log(S/K) + (r - delta + sigma^2/2)*T) / (sigma*sqrt(T))
     d2 <- d1 - sigma*sqrt(T)
     price <- -S * pnorm(-d1) + K*exp(-r*T)*pnorm(-d2)
