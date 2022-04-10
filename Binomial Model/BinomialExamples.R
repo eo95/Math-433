25b6f548eea84b43abae80ef4326c09ef69de192
Ethan O'Malley <69532110+eo95@users.noreply.github.com>
2022-04-09 20:01:24 -0400

    Convergence, Fixed TRG, Fixed small notational issues

--- Binomial Model/BinomialExamples.R
+++ Binomial Model/BinomialExamples.R
@@ -2,14 +2,77 @@
 
 # Pole Exercises: ----
 
+# Convergence
+x <- c()
+bs <- rep(4.0733,64)
+y_1 <- c()
+y_2 <- c()
+y_3 <- c()
+y_4 <- c()
+y_5 <- c()
+for (i in 1:64){
+  k = 8*i
+  x      <- append(x,k)
+  P_CRR  <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=2,n=k)
+  y_1    <- append(y_1, binomial_pricing(P_CRR,payoff=put_payoff)[[2]][1])
+  P_JR   <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=3,n=k)
+  y_2    <- append(y_2, binomial_pricing(P_JR,payoff=put_payoff)[[2]][1])
+  P_MCRR <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=4,n=k)
+  y_3    <- append(y_3, binomial_pricing(P_MCRR,payoff=put_payoff)[[2]][1])
+  P_EQP  <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=5,n=k)
+  y_4    <- append(y_4, binomial_pricing(P_EQP,payoff=put_payoff)[[2]][1])
+  P_TRG  <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=6,n=k)
+  y_5    <- append(y_5, binomial_pricing(P_TRG,payoff=put_payoff)[[2]][1])
+}
+plot(x,y_1, col="red")
+lines(x,bs)
+plot(x,y_2, col="green")
+lines(x,bs)
+plot(x,y_3, col="blue")
+lines(x,bs)
+plot(x,y_4, col="yellow")
+lines(x,bs)
+plot(x,y_5, col="black")
+lines(x,bs)
+x <- c()
+y_1 <- c()
+y_2 <- c()
+y_3 <- c()
+y_4 <- c()
+y_5 <- c()
+  for (i in 1:64){
+    k = 8*i
+    x      <- append(x,k)
+    C_CRR  <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=2,n=k)
+    y_1    <- append(y_1, binomial_pricing(P_CRR,payoff=call_payoff)[[2]][1])
+    C_JR   <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=3,n=k)
+    y_2    <- append(y_2, binomial_pricing(P_JR,payoff=call_payoff)[[2]][1])
+    C_MCRR <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=4,n=k)
+    y_3    <- append(y_3, binomial_pricing(P_MCRR,payoff=call_payoff)[[2]][1])
+    C_EQP  <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=5,n=k)
+    y_4    <- append(y_4, binomial_pricing(P_EQP,payoff=call_payoff)[[2]][1])
+    C_TRG  <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=6,n=k)
+    y_5    <- append(y_4, binomial_pricing(P_TRG,payoff=call_payoff)[[2]][1])
+  }
+plot(x,y_1, col="red")
+lines(x,bs)
+plot(x,y_2, col="green")
+lines(x,bs)
+plot(x,y_3, col="blue")
+lines(x,bs)
+plot(x,y_4, col="yellow")
+lines(x,bs)
+plot(x,y_5, col="black")
+lines(x,bs)
+
 # Problem 2
 # (a)
-P_CRR.256  <- generate_P(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=2,n=256)
-P_CRR.512  <- generate_P(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=2,n=512)
-P_JR.256   <- generate_P(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=3,n=256)
-P_JR.512   <- generate_P(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=3,n=512)
-P_MCRR.256 <- generate_P(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=4,n=256)
-P_MCRR.512 <- generate_P(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=4,n=512)
+P_CRR.256  <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=2,n=256)
+P_CRR.512  <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=2,n=512)
+P_JR.256   <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=3,n=256)
+P_JR.512   <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=3,n=512)
+P_MCRR.256 <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=4,n=256)
+P_MCRR.512 <- parameterize(T_exp=1,S=5,K=10,r=0.12,sigma=0.5,choice=4,n=512)
 
 C_1 <- binomial_pricing(P_CRR.256,payoff=put_payoff)[[2]][1]
 C_2 <- binomial_pricing(P_CRR.512,payoff=put_payoff)[[2]][1]
@@ -23,12 +86,12 @@ rownames(sol_2a) <- c("CRR","JR","MCRR")
 colnames(sol_2a) <- c("N=256","N=512")
 
 # (b)
-P_CRR.256  <- generate_P(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=2,n=256)
-P_CRR.512  <- generate_P(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=2,n=512)
-P_JR.256   <- generate_P(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=3,n=256)
-P_JR.512   <- generate_P(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=3,n=512)
-P_MCRR.256 <- generate_P(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=4,n=256)
-P_MCRR.512 <- generate_P(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=4,n=512)
+P_CRR.256  <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=2,n=256)
+P_CRR.512  <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=2,n=512)
+P_JR.256   <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=3,n=256)
+P_JR.512   <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=3,n=512)
+P_MCRR.256 <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=4,n=256)
+P_MCRR.512 <- parameterize(T_exp=0.25,S=60,K=65,r=0.08,sigma=0.3,choice=4,n=512)
 
 C_1 <- binomial_pricing(P_CRR.256,payoff=call_payoff)[[2]][1]
 C_2 <- binomial_pricing(P_CRR.512,payoff=call_payoff)[[2]][1]
@@ -47,34 +110,34 @@ sol_2b
 
 # Figure 10.4
 
-P_fig10.4 <- generate_P(S=41,K=40,sigma=0.3,r=0.08,T_exp=2,n=2,choice=1)
+P_fig10.4 <- parameterize(S=41,K=40,sigma=0.3,r=0.08,T_exp=2,n=2,choice=1)
 binomial_pricing(P_fig10.4,payoff=call_payoff)
 
 # Figure 10.5
 
-P_fig10.5 <- generate_P(S=41,K=40,sigma=0.3,r=0.08,T_exp=1,n=3,choice=1)
+P_fig10.5 <- parameterize(S=41,K=40,sigma=0.3,r=0.08,T_exp=1,n=3,choice=1)
 binomial_pricing(P_fig10.5,payoff=call_payoff)
 
 # Figure 10.6
 
-P_fig10.6 <- generate_P(S=41,K=40,sigma=0.3,r=0.08,T_exp=1,n=3,choice=1)
+P_fig10.6 <- parameterize(S=41,K=40,sigma=0.3,r=0.08,T_exp=1,n=3,choice=1)
 binomial_pricing(P_fig10.6,payoff=put_payoff)
 
 # Figure 10.7
 
-P_fig10.7 <- generate_P(S=41,K=40,sigma=0.3,r=0.08,T_exp=1,n=3,choice=1,eur=F)
+P_fig10.7 <- parameterize(S=41,K=40,sigma=0.3,r=0.08,T_exp=1,n=3,choice=1,eur=F)
 binomial_pricing(P_fig10.7,payoff=put_payoff)
 
 # Figure 10.8
 
-P_fig10.8 <- generate_P(S=110,K=100,sigma=0.3,r=0.05,T_exp=1,n=3,choice=1,eur=F,delta=0.035)
+P_fig10.8 <- parameterize(S=110,K=100,sigma=0.3,r=0.05,T_exp=1,n=3,choice=1,eur=F,delta=0.035)
 binomial_pricing(P_fig10.8,payoff=call_payoff)
 
 # McDonald Problems: ----
 
 # Problem 10.1
 
-P_10.1 <- generate_P(S=100,K=105,r=0.08,T_exp=0.5,u=1.3,d=0.8,n=1)
+P_10.1 <- parameterize(S=100,K=105,r=0.08,T_exp=0.5,u=1.3,d=0.8,n=1)
 
 #(a)
 binomial_pricing(P_10.1,payoff=call_payoff)
@@ -83,25 +146,25 @@ binomial_pricing(P_10.1,payoff=call_payoff)
 binomial_pricing(P_10.1,payoff=put_payoff)
 
 # Problem 10.2
-P_10.2 <- generate_P(S=100,K=95,r=0.08,T_exp=0.5,u=1.3,d=0.8,n=1)
+P_10.2 <- parameterize(S=100,K=95,r=0.08,T_exp=0.5,u=1.3,d=0.8,n=1)
 binomial_pricing(P_10.2,payoff=call_payoff)[[2]][1]
 
 # Problem 10.3
 
-P_10.3 <- generate_P(S=100,K=95,r=0.08,T_exp=0.5,u=1.3,d=0.8,n=1)
+P_10.3 <- parameterize(S=100,K=95,r=0.08,T_exp=0.5,u=1.3,d=0.8,n=1)
 binomial_pricing(P_10.3,payoff=put_payoff)[[2]][1]
 
 # Problem 10.6
 
-P_10.6 <- generate_P(S=100,K=95,r=0.08,T_exp=1,u=1.3,d=0.8,n=2)
+P_10.6 <- parameterize(S=100,K=95,r=0.08,T_exp=1,u=1.3,d=0.8,n=2)
 binomial_pricing(P_10.6,payoff=call_payoff)
 
 # Problem 10.8
 
-P_10.8 <- generate_P(S=100,K=95,r=0.08,T_exp=1,u=1.3,d=0.8,n=2)
+P_10.8 <- parameterize(S=100,K=95,r=0.08,T_exp=1,u=1.3,d=0.8,n=2)
 binomial_pricing(P_10.8,payoff=put_payoff)
 
 # Problem 10.10
 
-P_10.10 <- generate_P(S=100,K=95,r=0.08,T_exp=1,u=1.3,d=0.8,eur=F,n=2)
+P_10.10 <- parameterize(S=100,K=95,r=0.08,T_exp=1,u=1.3,d=0.8,eur=F,n=2)
 binomial_pricing(P_10.10,payoff=call_payoff)
--- Binomial Model/BinomialFunctions.R
+++ Binomial Model/BinomialFunctions.R
@@ -13,16 +13,15 @@ one_step_claim <- function(S,Su,Sd,Cu,Cd,r,h,delta=0,D=0) {
   return(c(C,A,B))
 }
 
-generate_S_m <- function(S,n,u,d,D_v) {
+generate_S_m <- function(S,n,u,d) {
   # Generates the matrix of S values for each node of the binomial tree using S = S[1,1], n, u, d
   # Output is S_m: an n+1 by n+1 numeric matrix.
   
   S_m <- matrix(0,n+1,n+1)
   S_m[1,1] <- S
   for(i in 1:n){
-    D <- D_v[i]
     S_a <- S_m[1:i,i]
-    S_b <- c(S_a[1]*u-D,S_a * d - D)
+    S_b <- c(S_a[1]*u,S_a*d)
     S_m[1:(i+1),i+1] <- S_b
   }
   return(S_m)
@@ -42,7 +41,7 @@ generate_S_v <- function(S,n,u,d,D_v){
   return(S_v)
 }
 
-generate_ud <- function(choice, h, r, delta, sigma, mu, K, S, n, u, d){
+generate_ud <- function(choice, h, r, delta, sigma, K, S, n, u, d){
   if(choice == 0){
     u <- u
     d <- d
@@ -66,13 +65,13 @@ generate_ud <- function(choice, h, r, delta, sigma, mu, K, S, n, u, d){
     d   <- exp(K_n-V_n)
   } else if(choice==5){
     #Equal Probabilities Tree (EQP)
-    a = mu*h
-    b = 0.5*sqrt(4*h*sigma^2 - 3*(h*mu)^2)
+    a = r*h
+    b = 0.5*sqrt(4*h*sigma^2 - 3*(h*r)^2)
     u = exp(0.5*a + b)
     d = exp(b - 1.5*a)
   } else if(choice==6){
     #Trigeorgis Tree (TRG)
-    a = sqrt(h*sigma^2 + (h*mu)^2)
+    a = sqrt(h*sigma^2 + (h*r)^2)
     u = exp(a)
     d = exp(-a)
   }
@@ -147,7 +146,7 @@ solve_binomial_pricing_recombine <- function(S_m,C_m,A_m,B_m,r,h,n,eur,payoff,K,
   
   for(i in seq(n,1,-1)){
     for(j in seq(1,i)){
-      values   <- one_step_claim(S_m[j,i],S_m[j,i+1],S_m[j+1,i+1],C_m[j,i+1],C_m[j+1,i+1],r,h,delta,D_v[i+1])
+      values   <- one_step_claim(S_m[j,i],S_m[j,i+1],S_m[j+1,i+1],C_m[j,i+1],C_m[j+1,i+1],r,h,delta)
       C_m[j,i] <- values[1]
       A_m[j,i] <- values[2]
       B_m[j,i] <- values[3]
@@ -185,13 +184,13 @@ binomial_pricing <- function(P,payoff){
   # Output is a list of matrices for the node values of S, C, A, B.
   recombine = P$recombine
   if(recombine){
-    S_m <- generate_S_m(P$S,P$n,u,d,D_v)
+    S_m <- generate_S_m(P$S,P$n,P$u,P$d)
     C_m <- matrix(0,P$n+1,P$n+1)
     D_m <- matrix(0,P$n+1,P$n+1)
     B_m <- matrix(0,P$n+1,P$n+1)
     S_T <- S_m[,P$n+1]
     C_m[,P$n+1] <- payoff(S_T,P$K)
-    solution <- solve_binomial_pricing_recombine(S_m,C_m,D_m,B_m,P$r,h,P$n,P$eur,payoff=payoff,P$K,P$delta,P$D_v)
+    solution <- solve_binomial_pricing_recombine(S_m,C_m,D_m,B_m,P$r_v,P$h,P$n,P$eur,payoff=payoff,P$K,P$delta,P$D_v)
   } else {
     S_v <- generate_S_v(P$S,P$n,P$u,P$d,P$D_v)
     n <- P$n
@@ -208,7 +207,7 @@ binomial_pricing <- function(P,payoff){
   return(solution)
 }
 
-parameterize <- function(S,r,T_exp=0,n=0,h=0,K,sigma=0.1,delta=0,mu=0,choice=0,u=10^8,d=10^8,eur=T,D_CF='None',recombine = T){
+parameterize <- function(S,r,T_exp=0,n=0,h=0,K,sigma=0.1,delta=0,choice=0,u=10^8,d=10^8,eur=T,D_CF='None',recombine = T){
   # S: Time 0 value of the asset priced in desired currency
   # r: Risk-free rate with respect to desired currency in decimal form
   #   may be given in the form of an integer, a vector, or a function
@@ -238,27 +237,35 @@ parameterize <- function(S,r,T_exp=0,n=0,h=0,K,sigma=0.1,delta=0,mu=0,choice=0,u
   else {h=T_exp/n}
   
   # Handles r_v:
-  if(recombine){
+  if(!is.function(r)){
     r_v <- rep(r,n)
   } else {
     r_v <- discretize_r_t(r,n,h)
   }
+  if(recombine){
+    r_v = r
+  }
   
   #vectorize delta
-  delta <- rep(delta,n)
+  if(!recombine){
+    delta <- rep(delta,n)
+  }
+  
   
   #vectorize sigma
-  sigma <- rep(sigma,n)
+  if(!recombine){
+    sigma <- rep(sigma,n)
+  }
   
   # Creates D_v
-  if(D_CF[1] == 'None'){
+  if(D_CF == 'None'){
     D_v <- rep(0,n)
   } else {
     D_v <- generate_D_v(D_CF,n,h,r_v)
   }
   
   # Creates u and d
-  ud <- generate_ud(choice,h,r_v,delta,sigma,mu,K,S,n,u,d)
+  ud <- generate_ud(choice,h,r_v,delta,sigma,K,S,n,u,d)
   u <- ud$u
   d <- ud$d
   
