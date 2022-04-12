8065aae0394e05fd4c1177083ce4d1be12597f17
OfficeOfThePope <99690263+OfficeOfThePope@users.noreply.github.com>
2022-04-12 13:48:25 -0400

    Update BinomialFunctions.R
    Fixed error in binomialPricing which treated r as a vector in recomibining model

--- Binomial Model/BinomialFunctions.R
+++ Binomial Model/BinomialFunctions.R
@@ -184,13 +184,15 @@ binomial_pricing <- function(P,payoff){
   # Output is a list of matrices for the node values of S, C, A, B.
   recombine = P$recombine
   if(recombine){
-    S_m <- generate_S_m(P$S,P$n,P$u,P$d)
+    u <- P$u[1]
+    d <- P$d[1]
+    S_m <- generate_S_m(P$S,P$n,u,d,P$D_v)
     C_m <- matrix(0,P$n+1,P$n+1)
     D_m <- matrix(0,P$n+1,P$n+1)
     B_m <- matrix(0,P$n+1,P$n+1)
     S_T <- S_m[,P$n+1]
     C_m[,P$n+1] <- payoff(S_T,P$K)
-    solution <- solve_binomial_pricing_recombine(S_m,C_m,D_m,B_m,P$r_v,P$h,P$n,P$eur,payoff=payoff,P$K,P$delta,P$D_v)
+    solution <- solve_binomial_pricing_recombine(S_m,C_m,D_m,B_m,P$r[1],h,P$n,P$eur,payoff=payoff,P$K,P$delta,P$D_v)
   } else {
     S_v <- generate_S_v(P$S,P$n,P$u,P$d,P$D_v)
     n <- P$n
