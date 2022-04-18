10d9a784a1cfd7cc584f9dff381e07097435bcb0
garvinorig <99909053+garvinorig@users.noreply.github.com>
2022-04-18 14:51:42 -0400

    Update Call.R

--- Black Scholes/Call.R
+++ Black Scholes/Call.R
@@ -1,7 +1,7 @@
 Call <- function(S, K, sigma, r, T_exp, delta) {
-  d1 <- (log(S/K) + (r + sigma^2/2)*T) / (sigma*sqrt(T))
-  d2 <- d1 - sigma*sqrt(T)
-  S *exp(-delta*T_exp)* pnorm(d1) - K*exp(-r*T)*pnorm(d2)
+  d1 <- (log(S/K) + (r + sigma^2/2)*T) / (sigma*sqrt(T_exp))
+  d2 <- d1 - sigma*sqrt(T_exp)
+  S *exp(-delta*T_exp)* pnorm(d1) - K*exp(-r*T_exp)*pnorm(d2)
   
 }
 
