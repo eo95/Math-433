66341fdf889d577f17fe12aeb0312b224541288b
garvinorig <99909053+garvinorig@users.noreply.github.com>
2022-04-18 14:51:18 -0400

    Update Put.R

--- Black Scholes/Put.R
+++ Black Scholes/Put.R
@@ -1,6 +1,6 @@
-Put <- function(S, K, sigma, r, T){
-  d1 <- (log(S/K) + (r + sigma^2/2)*T) / (sigma*sqrt(T))
+Put <- function(S, K, sigma, r, T_exp, delta){
+  d1 <- (log(S/K) + (r + sigma^2/2)*T_exp) / (sigma*sqrt(T_exp))
   d2 <- d1 - sigma*sqrt(T)
-  -S * pnorm(-d1) + K*exp(-r*T)*pnorm(-d2)
+  -S *exp(-delta*T_exp)* pnorm(-d1) + K*exp(-r*T)*pnorm(-d2)
 }
 
