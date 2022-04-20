f428d871927acab6d2f5b4bcbcbf9f5ff51af225
OfficeOfThePope <99690263+OfficeOfThePope@users.noreply.github.com>
2022-04-20 12:45:45 -0400

    Update ExoticCodes.R
    Table 14.1 in McDonald

--- Black Scholes/ExoticCodes.R
+++ Black Scholes/ExoticCodes.R
@@ -103,3 +103,72 @@ bsExchangePrice <- function(S,sigma_S,delta_S,K,sigma_K,delta_K,corr,T_exp){
   d2      <- d1 - sigma*sqrt(T_exp)
   price   <- S_piece*pnorm(d1) - K_piece*pnorm(d2)
 }
+
+asianAveragePriceOptions_bs <- function(P,N="Infinity"){
+  # Uses Equations in Appendix 14.A to price an asian option.
+  # Asian option calculated with Geometric Average as S_T, and at N evenly spaced time intervals in [0,T_exp]
+  r     = P$r
+  delta = P$delta
+  sigma = P$sigma
+  if(N == "Infinity"){
+    deltaNew = (r+delta+sigma^2/6)/2
+    sigmaNew = sigma * sqrt(1/3)
+  } else {
+    deltaNew = 0.5*(r*(N-1)/N+(delta+sigma^2/2)*(N+1)/N-sigma^2/N^2*(N+1)*(2*N+1)/(6))
+    sigmaNew = sigma/N*sqrt((N+1)*(2*N+1)/(6))
+  }
+  P$delta = deltaNew
+  P$sigma = sigmaNew
+  return(black_scholes(P))
+}
+
+asianAverageStrikeOptions_bs <- function(P,N="Infinity"){
+  # Uses Equations in Appendix 14.A to price an asian option.
+  # Asian option calculated with Geometric Average as K, and at N evenly spaced time intervals in [0,T_exp]
+  if(N=="Infinity"){N = 10^8}
+  r        = P$r
+  delta    = P$delta
+  sigma    = P$sigma
+  rNew     = 0.5*(r*(N-1)/N+(delta+sigma^2/2)*(N+1)/N-sigma^2/N^2*(N+1)*(2*N+1)/(6))
+  corr     = 0.5*sqrt(6*(N+1)/(2*N+1))
+  piece    = (N+1)*(2*N+1)/(6)/(N^2)
+  sigmaNew = sigma*sqrt(1+piece-2*corr*sqrt(piece))
+  strikeNew = P$S
+  P$r     = rNew
+  P$sigma = sigmaNew
+  P$K     = strikeNew
+  return(black_scholes(P))
+}
+
+# Uses two functions above along with black_scholes() to create table 14.1 in McDonald
+
+P.Call <- list(S=40,K=40,r=.08,sigma=0.3,delta=0,T_exp=1,put=F)
+P.Put <- list(S=40,K=40,r=.08,sigma=0.3,delta=0,T_exp=1,put=T)
+theNs <- c(1,2,3,5,10,50,1000)
+AveragePriceCallPrice  <- c()
+AveragePricePutPrice   <- c()
+AverageStrikeCallPrice <- c()
+AverageStrikePutPrice  <- c()
+for(i in theNs){
+  AvgPriceCallPrem   <-  asianAveragePriceOptions_bs(P.Call,N=i)
+  AvgPricePutPrem    <-  asianAveragePriceOptions_bs(P.Put, N=i)
+  AvgStrikeCallPrem  <- asianAverageStrikeOptions_bs(P.Call,N=i)
+  AvgStrikePutPrem   <- asianAverageStrikeOptions_bs(P.Put, N=i)
+  AveragePriceCallPrice <- c(AveragePriceCallPrice,AvgPriceCallPrem)
+  AveragePricePutPrice   <- c(AveragePricePutPrice,AvgPricePutPrem)
+  AverageStrikeCallPrice <- c(AverageStrikeCallPrice,AvgStrikeCallPrem)
+  AverageStrikePutPrice  <- c(AverageStrikePutPrice,AvgStrikePutPrem)
+}
+AvgPriceCallPrem   <-  asianAveragePriceOptions_bs(P.Call,N="Infinity")
+AvgPricePutPrem    <-  asianAveragePriceOptions_bs(P.Put, N="Infinity")
+AvgStrikeCallPrem  <- asianAverageStrikeOptions_bs(P.Call,N="Infinity")
+AvgStrikePutPrem   <- asianAverageStrikeOptions_bs(P.Put, N="Infinity")
+AveragePriceCallPrice <- c(AveragePriceCallPrice,AvgPriceCallPrem)
+AveragePricePutPrice   <- c(AveragePricePutPrice,AvgPricePutPrem)
+AverageStrikeCallPrice <- c(AverageStrikeCallPrice,AvgStrikeCallPrem)
+AverageStrikePutPrice  <- c(AverageStrikePutPrice,AvgStrikePutPrem)
+
+theNs <- c(theNs,"Infinity")
+table14.1 <- data.frame(theNs,AveragePriceCallPrice,AveragePricePutPrice,AverageStrikeCallPrice,AverageStrikePutPrice)
+colnames(table14.1) <- c("N","AvgPriceCall","AvgPricePut","AvgStrikeCall","AvgStrikePut")
+table14.1
