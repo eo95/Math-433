9aa19c94a73bc0405f766cc3bbf8bf6fdd20ae65
Ethan O'Malley <69532110+eo95@users.noreply.github.com>
2022-04-26 10:48:20 -0400

    fixed applications

--- Binomial Model/AppliedToCompanies.R
+++ Binomial Model/AppliedToCompanies.R
@@ -3,13 +3,15 @@ head(tesco.data)
 summary(tesco.data)
 tesco.sigma <- ann_volatility(as.numeric(tesco.data$Open[1:(252)]))
 
-tesco.P <- parameterize(S=277.5,r=.004452,T_exp=52/252,n=20,K=270,sigma=tesco.sigma,choice=4,eur=F)
+tesco.P <- parameterize(S=277.5,r=.004452,T_exp=52/252,n=20,K=270,sigma=tesco.sigma,choice=2,eur=F,recombine = F)
+tesco.P.bs <- bs_parameterize(S=277.5,r=.004452,T_exp=52/252,K=270,sigma=tesco.sigma,eur=F,put=F)
+tesco.model <- binomial_pricing(tesco.P,payoff=call_payoff)
+tesco.model.bs <- black_scholes(tesco.P.bs)
+tesco.model[[2]][1]
+tesco.P <- parameterize(S=277.5,r=.004452,T_exp=52/252,n=20,K=280,sigma=tesco.sigma,choice=2,eur=F,recombine = F)
 tesco.model <- binomial_pricing(tesco.P,payoff=call_payoff)
 tesco.model[[2]][1]
-tesco.P <- parameterize(S=277.5,r=.004452,T_exp=52/252,n=20,K=280,sigma=tesco.sigma,choice=4,eur=F)
-tesco.model <- binomial_pricing(tesco.P,payoff=call_payoff)
-tesco.model[[2]][1]
-tesco.P <- parameterize(S=277.5,r=.004452,T_exp=52/252,n=20,K=290,sigma=tesco.sigma,choice=4,eur=F)
+tesco.P <- parameterize(S=277.5,r=.004452,T_exp=52/252,n=20,K=290,sigma=tesco.sigma,choice=2,eur=F,recombine = F)
 tesco.model <- binomial_pricing(tesco.P,payoff=call_payoff)
 tesco.model[[2]][1]
 
@@ -19,13 +21,14 @@ summary(adobe.data)
 adobe.sigma <- ann_volatility(adobe.data$Open[1:(252*3)])
 
 
-adobe.P <- parameterize(S=452.13,r=.0033,T_exp=54/252,n=20,K=440,sigma=adobe.sigma,choice=4,eur=F)
+adobe.P <- parameterize(S=452.13,r=.0033,T_exp=54/252,n=20,K=440,sigma=adobe.sigma,choice=2,eur=F,recombine = F)
+adobe.P.bs <- bs_parameterize(S=452.13,r=.0033,T_exp=54/252,K=440,sigma=adobe.sigma,eur=F,put=F)
 adobe.model <- binomial_pricing(adobe.P,payoff=call_payoff)
 adobe.model[[2]][1]
-adobe.P <- parameterize(S=452.13,r=.0033,T_exp=54/252,n=20,K=450,sigma=adobe.sigma,choice=4,eur=F)
+adobe.P <- parameterize(S=452.13,r=.0033,T_exp=54/252,n=20,K=450,sigma=adobe.sigma,choice=2,eur=F,recombine = F)
 adobe.model <- binomial_pricing(adobe.P,payoff=call_payoff)
 adobe.model[[2]][1]
-adobe.P <- parameterize(S=452.13,r=.0033,T_exp=54/252,n=20,K=460,sigma=adobe.sigma,choice=4,eur=F)
+adobe.P <- parameterize(S=452.13,r=.0033,T_exp=54/252,n=20,K=460,sigma=adobe.sigma,choice=2,eur=F,recombine = F)
 adobe.model <- binomial_pricing(adobe.P,payoff=call_payoff)
 adobe.model[[2]][1]
 
@@ -34,13 +37,14 @@ head(nyt.data)
 summary(nyt.data)
 nyt.sigma <- ann_volatility(nyt.data$Open[1:(252)])
 
-nyt.P <- parameterize(S=46.01,r=.0033,T_exp=29/252,n=20,K=45,sigma=nyt.sigma,choice=4,D_CF=c(22/252,.09),eur=F)
+nyt.P <- parameterize(S=46.01,r=.0021,T_exp=29/252,n=20,K=45,sigma=nyt.sigma,choice=2,D_CF=c(22/252,.09),eur=F,recombine = F)
+nyt.P.bs <- bs_parameterize(S=46.01,r=.0033,T_exp=29/252,K=45,sigma=nyt.sigma,D_CF=c(22/252,.09),eur=F,put=F)
 nyt.model <- binomial_pricing(nyt.P,payoff=call_payoff)
 nyt.model[[2]][1]
-nyt.P <- parameterize(S=46.01,r=.0033,T_exp=29/252,n=20,K=46,sigma=nyt.sigma,choice=4,D_CF=c(22/252,.09),eur=F)
+nyt.P <- parameterize(S=46.01,r=.0021,T_exp=29/252,n=20,K=46,sigma=nyt.sigma,choice=2,D_CF=c(22/252,.09),eur=F,recombine = F)
 nyt.model <- binomial_pricing(nyt.P,payoff=call_payoff)
 nyt.model[[2]][1]
-nyt.P <- parameterize(S=46.01,r=.0033,T_exp=29/252,n=20,K=47,sigma=nyt.sigma,choice=4,D_CF=c(22/252,.09),eur=F)
+nyt.P <- parameterize(S=46.01,r=.0021,T_exp=29/252,n=20,K=47,sigma=nyt.sigma,choice=2,D_CF=c(22/252,.09),eur=F,recombine = F)
 nyt.model <- binomial_pricing(nyt.P,payoff=call_payoff)
 nyt.model[[2]][1]
 
@@ -49,13 +53,14 @@ head(novartis.data)
 summary(novartis.data)
 novartis.sigma <- ann_volatility(novartis.data$Open[1:(252)])
 
-novartis.P <- parameterize(S=77.64,r=-.0075,T_exp=53/252,n=20,K=76,sigma=novartis.sigma,choice=4,D_CF=c(3.1,4/252))
+novartis.P <- parameterize(S=77.64,r=-.0075,T_exp=53/252,n=20,K=76,sigma=novartis.sigma,choice=2,D_CF=c(4/252,3.1),recombine = F)
+novartis.P.bs <- bs_parameterize(S=77.64,r=-.0075,T_exp=53/252,K=76,sigma=novartis.sigma,D_CF=c(3.1,4/252),put=F)
 novartis.model <- binomial_pricing(novartis.P,payoff=call_payoff)
 novartis.model[[2]][1]
-novartis.P <- parameterize(S=77.64,r=-.0075,T_exp=53/252,n=20,K=78,sigma=novartis.sigma,choice=4,D_CF=c(3.1,4/252))
+novartis.P <- parameterize(S=77.64,r=-.0075,T_exp=53/252,n=20,K=78,sigma=novartis.sigma,choice=2,D_CF=c(4/252,3.1),recombine = F)
 novartis.model <- binomial_pricing(novartis.P,payoff=call_payoff)
 novartis.model[[2]][1]
-novartis.P <- parameterize(S=77.64,r=-.0075,T_exp=53/252,n=20,K=80,sigma=novartis.sigma,choice=4,D_CF=c(3.1,4/252))
+novartis.P <- parameterize(S=77.64,r=-.0075,T_exp=53/252,n=20,K=80,sigma=novartis.sigma,choice=2,D_CF=c(4/252,3.1),recombine = F)
 novartis.model <- binomial_pricing(novartis.P,payoff=call_payoff)
 novartis.model[[2]][1]
 
